ab7ed057bb4f18db8f33c0a2322e7c3c67909928
snowowl <snowowwwl@gmail.com>
2019-04-30 13:44:04 +0700

    task 9.1 done

---
+++ 09_functions/task_9_1b.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Задание 9.1b
+
+Сделать копию скрипта задания 9.1a.
+
+Изменить скрипт таким образом, чтобы функция возвращала не список команд, а словарь:
+    - ключи: имена интерфейсов, вида 'FastEthernet0/12'
+    - значения: список команд, который надо выполнить на этом интерфейсе:
+                          ['switchport mode access',
+                           'switchport access vlan 10',
+                           'switchport nonegotiate',
+                           'spanning-tree portfast',
+                           'spanning-tree bpduguard enable']
+
+
+Проверить работу функции на примере словаря access_dict,
+с генерацией конфигурации port-security и без.
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+"""
+
+
+def generate_access_config(access, psecurity=False):
+    """
+    access - словарь access-портов,
+    для которых необходимо сгенерировать конфигурацию, вида:
+        { 'FastEthernet0/12':10,
+          'FastEthernet0/14':11,
+          'FastEthernet0/16':17 }
+
+    psecurity - контролирует нужна ли настройка Port Security. По умолчанию значение False
+        - если значение True, то настройка выполняется с добавлением шаблона port_security
+        - если значение False, то настройка не выполняется
+
+    Функция возвращает словарь:
+    - ключи: имена интерфейсов, вида 'FastEthernet0/1'
+    - значения: список команд, который надо выполнить на этом интерфейсе
+    """
+
+    access_template = [
+        'switchport mode access', 'switchport access vlan',
+        'switchport nonegotiate', 'spanning-tree portfast',
+        'spanning-tree bpduguard enable'
+    ]
+
+    port_security = [
+        'switchport port-security maximum 2',
+        'switchport port-security violation restrict',
+        'switchport port-security'
+    ]
+
+    config_dict = {}
+    for info in access:
+        access_result = list()
+        access_result.append('interface {}'.format(info))
+        for command in access_template:
+            if command.endswith('vlan'):
+                access_result.append(command + ' {}'.format(access[info]))
+            else:
+                access_result.append(command)
+        if psecurity:
+            for command in port_security:
+                access_result.append(command)
+        else:
+            pass
+        config_dict[info] = access_result
+
+    return config_dict
+
+
+access_dict = {
+    'FastEthernet0/12': 10,
+    'FastEthernet0/14': 11,
+    'FastEthernet0/16': 17,
+    'FastEthernet0/17': 150
+}
+print(generate_access_config(access_dict, True))
