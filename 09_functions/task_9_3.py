6e0606b274dc7f3dac62b1ac708ff61e056a75a1
snowowl <snowowwwl@gmail.com>
2019-05-21 15:24:06 +0700

    task 9.3 done

--- 09_functions/task_9_3.py
+++ 09_functions/task_9_3.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Задание 9.3
 
 Создать функцию get_int_vlan_map, которая обрабатывает конфигурационный файл коммутатора
@@ -20,4 +20,28 @@
 
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
-'''
+"""
+
+
+def get_int_vlan_map(file):
+    """
+    This function reads configuration file and returns the dictionary of ports
+    with vlan numbers as a value
+    :param file: name of file
+    :return: dictionary of ports : 'FastEthernet0/1':[10,20]
+    """
+    port_dict = {}
+    port = str()
+    with open(file, 'r') as f:
+        for line in f:
+            if 'interface' in line:
+                port = line.lstrip('interface ').rstrip('\n')
+            elif 'access vlan' in line:
+                port_dict[port] = line.split()[-1]
+            elif 'trunk allowed vlan' in line:
+                vlans = line.split()[-1].split(',')
+                port_dict[port] = vlans
+    return port_dict
+
+
+print(get_int_vlan_map('C:/Users/snowowl/PycharmProjects/Natenka_python_tasks/09_functions/config_sw1.txt'))
--- 09_functions/task_9_3a.py
+++ 09_functions/task_9_3a.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Задание 9.3a
 
 Сделать копию скрипта задания 9.3.
@@ -22,4 +22,34 @@
 
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
-'''
+"""
+
+
+def get_int_vlan_map(file):
+    """
+    This function reads configuration file and returns the dictionary of ports
+    with vlan numbers as a value. If port is not configured, function will add such note in dictionary:
+    'FastEthernet0/20':1
+    :param file: name of file
+    :return: dictionary of ports : 'FastEthernet0/1':[10,20]
+    """
+    port_dict = {}
+    port = str()
+    with open(file, 'r') as f:
+        config_list = f.readlines()
+        for i in range(len(config_list)-1):
+            line = config_list[i]
+            nline = config_list[i+1]
+            if 'interface' in line:
+                port = line.lstrip('interface ').rstrip('\n')
+            elif 'trunk allowed vlan' in line:
+                vlans = line.split()[-1].split(',')
+                port_dict[port] = vlans
+            elif 'switchport mode access' in line and 'access vlan' in nline:
+                port_dict[port] = nline.split()[-1]
+            elif 'switchport mode access' in line and 'access vlan' not in nline:
+                port_dict[port] = '1'
+    return port_dict
+
+
+print(get_int_vlan_map('C:/Users/snowowl/PycharmProjects/Natenka_python_tasks/09_functions/config_sw2.txt'))
