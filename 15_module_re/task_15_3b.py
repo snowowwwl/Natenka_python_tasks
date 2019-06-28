870c58cdd122aca353d622ee1232e4c0cb113348
snowowl <snowowwwl@gmail.com>
2019-06-28 14:39:35 +0700

    15.3b

--- 15_module_re/task_15_3b.py
+++ 15_module_re/task_15_3b.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Задание 15.3b
 
 Проверить работу функции parse_cfg из задания 15.3a на конфигурации config_r2.txt.
@@ -23,4 +23,27 @@ Ethernet0/1 соответствует список из двух кортеже
 Обратите внимание, что в данном случае, можно не проверять корректность IP-адреса,
 диапазоны адресов и так далее, так как обрабатывается вывод команды, а не ввод пользователя.
 
-'''
+"""
+import re
+
+from pprint import pprint
+
+
+def parse_cfg(filename):
+    # return ip/mask in dict :
+    # {'FastEthernet0/1':('10.0.1.1', '255.255.255.0'),
+    #  'FastEthernet0/2':('10.0.2.1', '255.255.255.0')}#
+    regexpip = '(?:ip address (\d+\.\d+\.\d+\.\d+) (\d+\.\d+\.\d+\.\d+)\n){1,2}'
+    with open(filename) as f:
+        int_dict1={}
+        s = f.read()
+        s_list = re.split('!',s)
+        for line in s_list:
+            ipmasklist = re.findall(r'ip address (\d+\.\d+\.\d+\.\d+) (\d+\.\d+\.\d+\.\d+)', line)
+            interfaces = re.findall(r'interface (.+)\n\s+(?:.+\n\s+){0,2}ip address',line)
+            if interfaces:
+                int_dict1[str(interfaces).strip('[]').strip("'")] = ipmasklist
+    return int_dict1
+
+
+pprint(parse_cfg('config_r2.txt'))
