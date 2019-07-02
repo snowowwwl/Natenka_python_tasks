93c615a0b70d4a8bfd82aa4725ae21c3c3c6c591
snowowl <snowowwwl@gmail.com>
2019-07-02 14:22:14 +0700

    15.4

--- 15_module_re/task_15_4.py
+++ 15_module_re/task_15_4.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Задание 15.4
 
 Создать функцию parse_sh_ip_int_br, которая ожидает как аргумент
@@ -20,4 +20,25 @@
 
 Проверить работу функции на примере файла sh_ip_int_br_2.txt.
 
-'''
+"""
+import re
+from pprint import pprint
+
+
+def parse_sh_ip_int_br(filename):
+    """
+    Function takes information from the output of command "show ip interface brief"
+    and return list of tuples like:
+    [('FastEthernet0/0', '10.0.1.1', 'up', 'up'),
+     ('FastEthernet0/1', '10.0.2.1', 'up', 'up'),
+     ('FastEthernet0/2', 'unassigned', 'up', 'up')]
+    :param filename:
+    :return: list of tuples
+    """
+    regexpip = '(\S+) +(\d+\.\d+\.\d+\.\d+) +\S+ +\S+ +(\S+) +(\S+)\s+'
+    with open(filename) as f:
+        int_list = re.findall(regexpip, f.read())
+    return int_list
+
+if __name__ == "__main__":
+    pprint(parse_sh_ip_int_br('sh_ip_int_br_2.txt'))
--- 15_module_re/task_15_4a.py
+++ 15_module_re/task_15_4a.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Задание 15.4a
 
 Создать функцию convert_to_dict, которая ожидает два аргумента:
@@ -20,6 +20,28 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
-'''
+"""
 
+
+from pprint import pprint
+from task_15_4 import parse_sh_ip_int_br
 headers = ['interface', 'address', 'status', 'protocol']
+
+
+def convert_to_dict(list_of_headers, list_of_tuples):
+    """
+    Function takes list with keys and with parametres and returns a list of dict:
+    [{'interface': 'FastEthernet0/0', 'status': 'up', 'protocol': 'up', 'address': '10.0.1.1'},
+    {'interface': 'FastEthernet0/1', 'status': 'up', 'protocol': 'up', 'address': '10.0.2.1'}]
+    :param list_of_headers:
+    :param list_of_tuples:
+    :return: dict
+    """
+    list_of_dict_ip = []
+    for int_params in list_of_tuples:
+        dict_ip = dict(zip(list_of_headers, int_params))
+        list_of_dict_ip.append(dict_ip)
+    return list_of_dict_ip
+
+
+pprint(convert_to_dict(headers, parse_sh_ip_int_br('sh_ip_int_br_2.txt')))
