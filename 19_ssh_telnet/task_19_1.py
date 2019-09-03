d8c690c2175467ac5539a15c8f4bd20acf2a3759
snowowl <snowowwwl@gmail.com>
2019-09-03 14:56:51 +0700

    19.1

---
+++ 19_ssh_telnet/task_19_1.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 19.1
+
+Создать функцию send_show_command.
+
+Функция подключается по SSH (с помощью netmiko) к устройству и выполняет указанную команду.
+
+Параметры функции:
+* device - словарь с параметрами подключения к устройству
+* command - команда, которую надо выполнить
+
+Функция возвращает словарь с результатами выполнения команды:
+* ключ - IP устройства
+* значение - результат выполнения команды
+
+Отправить команду command на все устройства из файла devices.yaml (для этого надо считать информацию из файла) с помощью функции send_show_command.
+
+'''
+from netmiko import ConnectHandler
+import yaml
+from pprint import pprint
+
+
+
+with open('devices.yaml') as f:
+    devices = yaml.safe_load(f)
+pprint(devices)
+commands = ['interface',
+            'export compact']
+
+
+
+def send_show_command(device, commands):
+    result_dict = {}
+    with ConnectHandler(**device) as ssh:
+        ssh.enable()
+        result = ssh.send_config_set(commands)
+        key = device['ip']
+        result_dict[key] = result
+        return result_dict
+
+for device in devices['routers']:
+    pprint(send_show_command(device, commands))
+
+
