f52752c7d82432ba27ed48f9348124f1dde92890
snowowl <snowowwwl@gmail.com>
2019-09-07 13:44:12 +0700

    19.3

--- 19_ssh_telnet/task_19_3.py
+++ 19_ssh_telnet/task_19_3.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Задание 19.3
 
 Создать функцию send_commands (для подключения по SSH используется netmiko).
@@ -29,9 +29,75 @@
     * команды command
     * файла config.txt
 
-'''
+"""
 
-commands = [
-    'logging 10.255.255.1', 'logging buffered 20010', 'no logging console'
-]
-command = 'sh ip int br'
+
+import netmiko
+from netmiko import ConnectHandler
+import yaml
+from pprint import pprint
+
+
+commands = ['interface',
+            'export compact']
+command = 'interface'
+
+with open('devices.yaml') as f:
+    devices = yaml.safe_load(f)
+
+def send_show_command(dev, com):
+    result_dict = {}
+    with ConnectHandler(**dev) as ssh:
+        ssh.enable()
+        result = ssh.send_command(com)
+        key = dev['ip']
+        result_dict[key] = result
+        return result_dict
+
+def send_config_commands(dev, com):
+    result_dict = {}
+    with ConnectHandler(**dev) as ssh:
+        ssh.enable()
+        result = ssh.send_config_set(com)
+        key = dev['ip']
+        result_dict[key] = result
+        return result_dict
+
+def send_config_fromfile(dev, filenm):
+    result_dict = {}
+    with ConnectHandler(**dev) as ssh:
+        ssh.enable()
+        result = ssh.send_config_from_file(filenm)
+        key = dev['ip']
+        result_dict[key] = result
+        return result_dict
+
+def send_commands(device, show = None, config = None, filename = None):
+    result_dict = {}
+    if show and not config and not filename:
+        result_dict = send_show_command(device, show)
+    elif config and not show and not filename:
+        result_dict = send_config_commands(device, config)
+    elif filename and not show and not config:
+        result_dict = send_config_fromfile(device, filename)
+    else:
+        print('Please enter only one parametr show/filename/config')
+    return result_dict
+
+
+try:
+    for dev in devices['routers']:
+        print("Command:")
+        pprint(send_commands(dev, show = command))
+        print("Set of commands:")
+        pprint(send_commands(dev, config = commands))
+        print("Set of commands from file:")
+        pprint(send_commands(dev, filename = 'config_set.txt'))
+        print("Wrong parametr:")
+        pprint(send_commands(dev, show = command, config = commands))
+
+except netmiko.ssh_exception.NetMikoAuthenticationException:
+    print("Login or pass are not valid. Please check login/pass.")
+except netmiko.ssh_exception.NetMikoTimeoutException:
+    print("Connection to device {} timed-out".format(dev['ip']))
+
